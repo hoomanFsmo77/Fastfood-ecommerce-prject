fac063504fee38f72cfd9d7af614190022ae8582
hooman mousavi <hoomanmousavi77@gmail.com>
2023-06-13 21:26:00 +0330

    initial commit

---
+++ client/colors.ts
@@ -0,0 +1,11 @@
+
+export default {
+    'primary-light-1':'#f3f3f3', // body
+    'primary-light-2':'#7d7d7d',
+    'primary-dark-1':'#242424', /// text
+    'primary-dark-2':'#0f1924', /// btn
+    'secondary-light-1':'#f7b82d',
+    'secondary-light-2':'#a41a13',
+    'secondary-light-3':'#e93030',
+    'secondary-light-4':'#ccb265',
+}
